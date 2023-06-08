5cb7ca0b9410c58ce21274c19fbca64ead6880a2
Ddavila09 <ddavilax23@gmail.com>
2023-06-08 17:31:11 -0500

    DojoSurvery1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("results")]
+    public IActionResult Create (Student newStudent)
+    {
+        Console.WriteLine($"{newStudent.Name} studies in {newStudent.Location} and they love {newStudent.Language}. This is what they have to say {newStudent.Comment}");
+        return View("Submitted", newStudent);
+    }
+
+
+    [HttpGet("privacy")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet("{**path}")]
+    public RedirectResult Unknown()
+    {
+        return Redirect ("/");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+namespace DojoSurvey.Models;
+public class Student
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Language { get; set; }
+    public string Comment { get; set; }
+
+}
